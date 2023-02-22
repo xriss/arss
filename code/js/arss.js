d8dbaad0ae533878709f009fb854da8dcc26ad5e
kriss <kriss@xixs.com>
2023-02-22 15:07:52 +0000

    run the code

--- code/js/arss.js
+++ code/js/arss.js
@@ -8,7 +8,7 @@ let $ = require( "jquery" )
 arss.setup=function(args)
 {
 	window.arss=arss
-	tok.args=args || {}		// remember args
+	arss.args=args || {}		// remember args
 	$(arss.start)			// wait for page to load then call start
 }
 
