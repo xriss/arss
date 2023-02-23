d794e9c318cfa9307dfe371761d1bb28d4b18831
kriss <kriss@xixs.com>
2023-02-23 13:17:55 +0000

    .

--- code/js/arss.js
+++ code/js/arss.js
@@ -1,10 +1,14 @@
 
 var arss=exports
 
-
-
 let $ = require( "jquery" )
 
+
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+  
+  
 arss.setup=function(args)
 {
 	window.arss=arss
@@ -16,5 +20,19 @@ arss.start=async function()
 {
 
 	console.log("ARSS has started")
+
+	let url="https://edition.cnn.com/"
+	url="http://rss.cnn.com/rss/cnn_topstories.rss"
+	
+  let feed = await parser.parseURL(url);
+  console.log(feed);
+/*
+	$.get( url, function( data ) {
+	  console.log( url );
+	  console.log( data );
+	  $( "body" ).html( ""+data );
+	});
+*/
+
 }
 
